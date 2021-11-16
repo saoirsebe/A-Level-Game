545892fa4b2e9b2f957475a50500f637647839a5
saoirsebe <saoirse.be@gmail.com>
2021-11-16 12:43:18 +0000

    splitVeryically and Horizontally methods

--- ALevelGame/Assets/Scripts/BinarySP.cs
+++ ALevelGame/Assets/Scripts/BinarySP.cs
@@ -20,11 +20,11 @@ public static class BinarySP
                 {
                     if (room.size.y >= minHeight * 2)
                     {
-                        splitHorizontally(minWidth, minHeight, roomsQueue, room);
+                        splitHorizontally(minHeight, roomsQueue, room);
                     }
                     else if (room.size.x >= minWidth * 2)
                     {
-                        splitVertically(minWidth, minHeight, roomsQueue, room);
+                        splitVertically(minWidth, roomsQueue, room);
                     }
                     else
                     {
@@ -38,11 +38,11 @@ public static class BinarySP
                     
                     if (room.size.x >= minWidth * 2)
                     {
-                        splitVertically(minWidth, minHeight, roomsQueue, room);
+                        splitVertically(minWidth, roomsQueue, room);
                     }
                     else if (room.size.y >= minHeight * 2)
                     {
-                        splitHorizontally(minWidth, minHeight, roomsQueue, room);
+                        splitHorizontally(minHeight, roomsQueue, room);
                     }
                     else
                     {
@@ -54,15 +54,26 @@ public static class BinarySP
         return roomsList;
     }
 
-    private static void splitHorizontally(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    private static void splitHorizontally(int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
-        throw new System.NotImplementedException();
+        var ySplit = Random.Range(1, room.size.y);
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x, room.min.y + ySplit, room.min.z),
+            new Vector3Int(room.size.x, room.size.y - ySplit, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
     }
 
 
-    private static void splitVertically(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    private static void splitVertically(int minWidth, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
-        throw new System.NotImplementedException();
+        var xSplit = Random.Range(1, room.size.x);
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.min.y, room.min.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z),
+            new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
+
     }
 
 }
