0d7ff66dde32edb593b9fdae560da73db7b21635
saoirsebe <saoirse.be@gmail.com>
2021-11-16 12:51:00 +0000

    RoomFirst sctipt perameters

---
+++ ALevelGame/Assets/Scripts/RoomFirstGenerator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomFirstGenerator : MapGenerator
+{
+    [SerializeField]
+    private int minRoomWidth = 4, minRoomHeight = 4;
+    [SerializeField]
+    private int mapWidth = 20, mapHeight = 20;
+}
