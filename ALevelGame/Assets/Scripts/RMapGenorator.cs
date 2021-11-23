e5fc1017dfab7ca2ebb4ebde1c4cfbed928890df
saoirsebe <saoirse.be@gmail.com>
2021-11-23 13:17:05 +0000

    2 Level templates made with randomiser between them

--- ALevelGame/Assets/Scripts/RMapGenorator.cs
+++ ALevelGame/Assets/Scripts/RMapGenorator.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class RMapGenorator : MonoBehaviour
 {
+
+    public bool Spawn1;
+
     // Start is called before the first frame update
     void Start()
     {
+        int pickBool = Random.Range(0, 1);
         
+        if (pickBool == 0)
+        {
+            Spawn1 = true;
+        }
+        else
+        {
+            Spawn1 = false;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
