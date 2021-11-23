28785e647e129f987484943430b3255ceaee54f0
saoirsebe <saoirse.be@gmail.com>
2021-11-23 20:06:40 +0000

    LevelTemplated editing, added circles for spawn placement + Room 1 spawning away from spawn point?

--- ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
+++ ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
@@ -4,31 +4,21 @@ using UnityEngine;
 
 public class RoomSpawnPoint : MonoBehaviour
 {
-   // public GameObject RoomLocation { get; private set; }
-    //public GameObject RoomNew { get; private set; }
 
-    //public GameObject StartRoom;
+
 
     public GameObject[] objects;
 
     void Start()
     {
+
+
         int rand = Random.Range(0, objects.Length);
 
         Instantiate(objects[rand],transform.position, Quaternion.identity);
-
-       // StartCoroutine(NewRoom());
+     
     }
 
-    /* IEnumerator NewRoom()
-    {
-        RoomLocation = GameObject.Find("RoomParent");
-
-        RoomNew = Instantiate(original: StartRoom, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
-        RoomNew.transform.parent = transform;
-
-        yield return new WaitForSeconds(1);
-    }*/
 
     
 }
