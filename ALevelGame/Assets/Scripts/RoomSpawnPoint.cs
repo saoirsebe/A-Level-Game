e5b71c641dba354dde9c98d78e2fdb0f375cb709
saoirsebe <saoirse.be@gmail.com>
2021-11-25 22:01:12 +0000

    x and y coordinate variables for room and put into Room instance (not working)

--- ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
+++ ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
@@ -7,6 +7,8 @@ public class RoomSpawnPoint : MonoBehaviour
 {
     RMapGenorator s1;
 
+    public Room Room;
+
     public GameObject[] objects;
     void Start()
     {
@@ -18,8 +20,13 @@ public class RoomSpawnPoint : MonoBehaviour
 
     IEnumerator start()
     {
+        int locationx = (int)transform.position.x;
+        int locationy = (int)transform.position.y;
+
+
         s1 = GetComponent<RMapGenorator>();
-        s1.roomsList.Append(new Room());
+        
+        s1.roomsList.Append(new Room(locationx, locationy,));
 
         yield return new WaitForEndOfFrame();
     }
