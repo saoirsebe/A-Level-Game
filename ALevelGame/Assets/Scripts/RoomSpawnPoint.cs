0eb5d192c7dc7511e0c32c3450f6d869efceaa84
saoirsebe <saoirse.be@gmail.com>
2021-11-25 22:59:38 +0000

    Added objects with tag door to list

--- ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
+++ ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
@@ -10,18 +10,30 @@ public class RoomSpawnPoint : MonoBehaviour
     private List<Door> doors = new List<Door>();
 
     public GameObject[] objects;
+    private List <GameObject> doorsAdd =  new List<GameObject>();
+
     void Start()
     {
         int rand = Random.Range(0, objects.Length);
         Instantiate(objects[rand],transform.position, Quaternion.identity);//Spawns random room from list
+
+        foreach (GameObject doorObj in GameObject.FindGameObjectsWithTag("Door"))
+        {
+            doorsAdd.Add(doorObj);
+        }
+      
+
+        for (door in doorsAdd)
+        {
+
+        }
+
     }
 
     IEnumerator start()
     {
         int locationx = (int)transform.position.x;
         int locationy = (int)transform.position.y;
-
-
 
         s1 = GetComponent<RMapGenorator>();
         s1.roomsList.Append(new Room (locationx, locationy,doors));
