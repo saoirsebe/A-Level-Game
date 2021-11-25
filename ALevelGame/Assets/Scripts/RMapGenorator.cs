a76dd516e38d660aeada8b1d1b93c47b551c76c0
saoirsebe <saoirse.be@gmail.com>
2021-11-25 20:10:04 +0000

    Added blank room to roomsList at each spawn point

--- ALevelGame/Assets/Scripts/RMapGenorator.cs
+++ ALevelGame/Assets/Scripts/RMapGenorator.cs
@@ -9,59 +9,13 @@ public class RMapGenorator : MonoBehaviour
 
     public GameObject[] nodes;
 
-    /*
-    public List<Transform> findShortestPath(Transform start,Transform end)
-    {
-        nodes = GameObject.FindGameObjectsWithTag("Door");
-
-        List<Transform> result = new List<Transform>();
-        Transform node = dijkstrasAlgo(start, end);
-
-        while (node != null)
-        {
-            result.Add(node);
-            Node currentNode = node.GetComponent<Node>();
-            node = currentNode.getParentNode();
-        }
-        //result.Reverse();
-        return result;
+    void Start() 
+    { 
+        public List<Room> roomsList = new List<Room>();
     }
-
-    private Transform dijkstrasAlgo(Transform start, Transform end)
-    {
-        double startTime = Time.realtimeSinceStartup;
-
-        List<Transform> unexplored = new List<Transform>(); //unexplored nodes
-        foreach (GameObject obj in nodes)
-        {
-            Node n = obj.GetComponent<Node>();
-            if (n.isWalkable())
-            {
-                n.resetNode();
-                unexplored.Add(obj.transform);
-            }
-        }
-        Node startNode = start.GetComponent<Node>();
-        startNode.setWeight(0);
-
-        while (unexplored.Count > 0)
-        {
-            // Sort the explored by their weight in ascending order.
-            unexplored.Sort((x, y) => x.GetComponent<Node>().getWeight().CompareTo(y.GetComponent<Node>().getWeight()));
-
-            // Get the lowest weight in unexplored.
-            Transform current = unexplored[0];
-            unexplored.Remove(current);
-
-            Node currentNode = current.GetComponent<Node>();
-            List<Transform> neighbours = currentNode.getNeighbourNode();
-            foreach (Transform neighNode in neighbours)
-            {
-
-            }
-
-        }*/
-
+        
+    
+    
 
 
     public List<Vector2Int> shortestPathCoridors(Vector2Int startPosition, Vector2Int endPosition)
@@ -69,9 +23,6 @@ public class RMapGenorator : MonoBehaviour
         List<Vector2Int> corridors = new List<Vector2Int>();//List of corridors
 
    
-        
-
- 
 
 
         return corridors;
--- ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
+++ ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RoomSpawnPoint : MonoBehaviour
 {
+    RMapGenorator s1;
+
     public GameObject[] objects;
     void Start()
     {
         int rand = Random.Range(0, objects.Length);
 
         Instantiate(objects[rand],transform.position, Quaternion.identity);
-     
+
     }
+
+    IEnumerator start()
+    {
+        s1 = GetComponent<RMapGenorator>();
+        s1.roomsList.Append(new Room());
+
+        yield return new WaitForEndOfFrame();
+    }
+    
 
 }
