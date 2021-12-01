7c0acc3c48d8893399d866a484a2ae3c9a29b628
saoirsebe <saoirse.be@gmail.com>
2021-12-01 16:40:22 +0000

    Removed unneccesary code in RoomSpawnPoint

--- ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
+++ ALevelGame/Assets/Scripts/RoomSpawnPoint.cs
@@ -18,9 +18,11 @@ public class RoomSpawnPoint : MonoBehaviour
 
     void Start()
     {
+        int locationx = (int)transform.position.x;
+        int locationy = (int)transform.position.y;
+
         int rand = Random.Range(0, objects.Length);
         GameObject obj = Instantiate(objects[rand],transform.position, Quaternion.identity);//Spawns random room from list
-
 
         if (searchTag != null)
         {
@@ -28,31 +30,15 @@ public class RoomSpawnPoint : MonoBehaviour
             FindObjectswithTag(searchTag,obj);
         }
 
-
-
-        foreach (GameObject door in doorsAdd)
-        {
-            transPos = door.transform.position;
-
-            xCoord = (int)transPos.x;
-            yCoord = (int)transPos.y;
-
-            doors.Append(new Door(xCoord, yCoord,0));
-        }
-
-        int locationx = (int)transform.position.x;
-        int locationy = (int)transform.position.y;
-
         s1 = GetComponent<RMapGenorator>();
         s1.roomsList.Append(new Room(locationx, locationy, doors));
 
         Debug.Log("p");
-
     }
 
     private void FindObjectswithTag(string _tag,GameObject obj)
     {
-        doorsAdd.Clear();
+        doors.Clear();
         Transform parent = obj.transform;
         GetChildObject(parent, _tag);
     }
@@ -73,21 +59,6 @@ public class RoomSpawnPoint : MonoBehaviour
             }
         }
     }
-
-    /*IEnumerator Start()
-    {
-        int locationx = (int)transform.position.x;
-        int locationy = (int)transform.position.y;
-
-        s1 = GetComponent<RMapGenorator>();
-        s1.roomsList.Append(new Room (locationx, locationy,doors));
-
-        Debug.Log("p");
-       
-
-        yield return new WaitForEndOfFrame();
-
-    }*/
-    
+  
     
 }
